fe03c60705e7dba2a587de53e871ff2bfce90f88
Daniel Okonkwo <danielokonkwo.goz@gmail.com>
2025-06-11 13:38:03 -0500

    Finished North Wind Traders Part 3 - Try with Resources

--- NorthwindTraders/src/main/java/com/pluralsight/App.java
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 
 public class App {
     static Scanner scanner = new Scanner(System.in);
-    static Connection connection = null;
-    static PreparedStatement preparedStatement = null;
-    static ResultSet resultSet = null;
     static String formatter;
 
 
@@ -24,71 +21,78 @@ public class App {
         String username = args[0];
         String password = args[1];
 
-        boolean appRunning = true;
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", username, password)) {
 
-        while (appRunning) {
+            boolean appRunning = true;
 
-            try {
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind",
-                        username,
-                        password);
+            while (appRunning) {
 
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+                System.out.println("1) Display All Products");
+                System.out.println("2) View All customers");
+                System.out.println("3) Display all categories");
+                System.out.println("0) Exit");
+                System.out.println("Select an option: ");
 
+                int subOption = scanner.nextInt();
 
-            System.out.println("1) Display All Products");
-            System.out.println("2) View All customers");
-            System.out.println("0) Exit");
-            System.out.println("Select an option: ");
-
-            int subOption = scanner.nextInt();
-
-            switch (subOption) {
-                case 1:
-                    viewProducts(connection);
-                    break;
-                case 2:
-                    viewCustomers(connection);
-                    break;
-                case 0:
-                    System.out.println("All good Boss.");
-                    if (connection != null) {
-                        try {
-                            connection.close();
-                        } catch (SQLException e) {
-                            e.printStackTrace();
+                switch (subOption) {
+                    case 1:
+                        viewProducts(connection);
+                        break;
+                    case 2:
+                        viewCustomers(connection);
+                        break;
+                    case 3:
+                        categoryMenu(connection, scanner);
+                        break;
+                    case 0:
+                        System.out.println("All good Boss.");
+                        if (connection != null) {
+                            try {
+                                connection.close();
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                    appRunning = false;
-                    break;
+                        appRunning = false;
+                        break;
+
+                    default:
+                        System.out.println("Invalid Choice");
+
+                }
+
 
             }
-
-
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void viewProducts(Connection connection) {
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
 
-        try {
-            // create prepared statement
-            //this is like opening your query window
-            preparedStatement = connection.prepareStatement(
-                    "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM products");
+        try (
+                // create prepared statement
+                //this is like opening your query window
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT " +
+                                "ProductID, " +
+                                "ProductName, " +
+                                "UnitPrice, " +
+                                "UnitsInStock " +
+                                "FROM " +
+                                "products " +
+                                " ORDER BY " +
+                                "ProductName");
 
-            resultSet = preparedStatement.executeQuery();
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
 
             formatter = System.out.printf("%-4s %-20s %-8s %-6s\n", "Id", "Name", "Price", "Stock").toString();
-
             // process the results
             // this shows the way to view the results set but java doesn't have a spreadsheet view for us
             System.out.println(formatter);
             System.out.println("--------------------------------------------");
-
             // result loop for view
             while (resultSet.next()) {
                 int id = resultSet.getInt("ProductID");
@@ -97,27 +101,10 @@ public class App {
                 int unitStock = resultSet.getInt("UnitsInStock");
                 System.out.printf("%-4d %-20s  %-7.2f %-6d\n", id, productName, unitPrice, unitStock);
 
-
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            // close the resources
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
         }
 
     }
@@ -127,16 +114,23 @@ public class App {
         PreparedStatement preparedStatement1 = null;
         ResultSet resultSet1 = null;
 
-        try {
+        try (
 
-            preparedStatement = connection.prepareStatement(
-                    "SELECT " + " ContactName, " + " CompanyName, " +
-                            " City, " + "Country, " + " Phone " + "FROM" + " Customers " + "ORDER BY " +
-                            "Country");
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT " +
+                                " ContactName, " +
+                                " CompanyName, " +
+                                " City, " +
+                                "Country, " +
+                                " Phone " +
+                                "FROM" +
+                                " Customers " +
+                                "ORDER BY " +
+                                "Country");
 
 
-
-            resultSet = preparedStatement.executeQuery();
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
 
             formatter = System.out.printf("%-4s %-20s %-8s %-6s %-2s\n", "Contact Name", "Company Name", "City", "Country", "Phone").toString();
 
@@ -157,26 +151,65 @@ public class App {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            // close the resources
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+        }
+    }
 
+
+    public static void categoryMenu(Connection connection, Scanner scanner) {
+
+        //ask them if they want to see products for a specific category
+        System.out.println("Do you want to see the products in a category?");
+        System.out.println("\t1) Yes");
+        System.out.println("\t2) No");
+        System.out.print("Select an option: ");
+
+        switch (scanner.nextInt()) {
+            case 1:
+
+                System.out.print("What category id would you like to view products for?: ");
+                int catID = scanner.nextInt();
+                viewAllCategories(connection, catID);
+                break;
+            case 2:
+                return;
+            default:
+                System.out.println("Ok, back to the main menu then!");
         }
 
     }
+
+    public static void viewAllCategories(Connection connection, int catID) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+            "SELECT " +
+                    " CategoryID, " +
+                    " CategoryName " +
+                "FROM " +
+                    " Categories " +
+                "ORDER BY " +
+                    " CategoryId");
+
+             ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            formatter = System.out.printf("%-4s %-20s \n", "Category ID", "Categories").toString();
+            // process the results
+            // this shows the way to view the results set but java doesn't have a spreadsheet view for us
+            System.out.println(formatter);
+            System.out.println("--------------------------------------------");
+
+            // result loop for view
+            while (resultSet.next()) {
+                int categoryID = resultSet.getInt("CategoryID");
+                String categoryName = resultSet.getString("CategoryName");
+                System.out.printf("%-4s %-20s \n", categoryID, categoryName);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+
+
 
 
